47b914e576d84676a76128e6c0abad40524f3490
Quyet Vu <72632257+quyet-v@users.noreply.github.com>
2025-03-19 21:17:01 +1300

    BEP-4 Generated migrations (#3)

---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace be_patient_api;
+
+public class User
+{
+    [Key]
+    public int Id {get; set;}
+
+    public string FirstName {get; set;}
+
+    public string LastName {get; set;}
+}
--- src/Infrastructure/Persistence/AppDbContext.cs
+++ src/Infrastructure/Persistence/AppDbContext.cs
@@ -4,4 +4,8 @@ namespace be_patient_api;
 
 public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> context) : base(context) { }
+
+    public DbSet<User> Users {get; set;}
+
 }
